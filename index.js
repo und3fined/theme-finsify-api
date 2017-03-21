a788910714db4c5cdf219f84508296bc72b2d084
CuongLe <cuongle.kty@gmail.com>
2017-03-21 15:30:51 +0700

    Init

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 module.exports = {
     hooks: {
         config: function(config) {
-            config.styles = config.styles || config.pluginsConfig['theme-default'].styles;
+            config.styles = config.styles || config.pluginsConfig['theme-finsify-api'].styles;
 
             return config;
         }
